0d9215ef8ad7408f09c5bc526dbe718271a9c81d
baart22-oss <baart22@gmail.com>
2026-03-05 11:17:53 +0200

    Import clearToken and User type in auth.tsx

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { api } from './client';
+import { api, clearToken, type User } from './client';
 
 interface AuthContextType {
   user: User | null;
