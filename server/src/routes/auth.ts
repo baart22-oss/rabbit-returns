b484e63016143e0e756c08198291dde2462fd5c6
baart22-oss <baart22@gmail.com>
2026-03-07 15:34:49 +0200

    Refactor auth.ts to use Router from express

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,3 +1,6 @@
+import { Router } from "express";
+const router = Router();
+export default router;
 import { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
